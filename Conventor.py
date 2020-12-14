c2be6fdc07f9efceb5f8214e11410f3900f0eac7
minsittpaingmhuu <71578371+minsittpaingmhuu@users.noreply.github.com>
2020-12-14 12:59:53 +0630

    Update Conventor.py

--- Conventor.py
+++ Conventor.py
@@ -62,7 +62,7 @@ def fun():
 
     else:
         print("!!--------Invaild Option (",ask,")--------!!")
-        fun1()
+        fun()
 
 def fun1():
     ask1 = input("Want to do it one more time ? (Type y/n) : ")
